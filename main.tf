7d4bbe9c3cb9c4078b5d1cdcbae028447b4f854d
gezm0 <eremin@ryazan.net.ru>
2022-03-16 16:39:34 +0300

    terraform lesson 1 homework

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
             version = "~> 4.5"
         }
     }
-    required_version = "~> 1.1.6"
+    required_version = "~> 1.0"
 }
 
 provider "aws" {
@@ -17,15 +17,40 @@ data "aws_vpc" "default" {
 }
 
 data "aws_security_group" "Default_VPC" {
-  vpc_id = data.aws_vpc.default.id
+    vpc_id = data.aws_vpc.default.id
 
-  filter {
+filter {
     name   = "group-name"
     values = ["default"]
-  }
+    }
 }
 
-output "aws_security_group_id" {
+output "aws_default_vpc_id" {
     value = data.aws_vpc.default.id
     description = "Default VPC id"
+}
+
+data "aws_subnet_ids" "default" {
+    vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_subnet" "default" {
+    for_each = data.aws_subnet_ids.default.ids
+    id       = each.value
+}
+
+output "subnet_cidr_blocks" {
+    value = [for s in data.aws_subnet.default : s.cidr_block]
+}
+
+data "aws_security_groups" "default" {
+filter {
+    name   = "group-name"
+    values = ["default"]
+    }
+}
+
+output "aws_security_groups" {
+    value = data.aws_security_groups.default.ids
+    description = "Default security groups"
 }
